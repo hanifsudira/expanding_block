555e91b29bda860e48a7c669e02d365575c1fcf8
Efron <Efron_Licht@yahoo.com>
2016-02-20 00:00:22 -0800

    specified output, variable renaming for consistency, added process_bucket

---
+++ block.m
@@ -0,0 +1,15 @@
+classdef block
+    properties
+        pixels
+        N       %dimension
+        avg_gray
+        variance
+        std_deviation
+        xy
+    end
+    methods
+        function obj = initialize(init)
+            
+        end
+    end
+end
